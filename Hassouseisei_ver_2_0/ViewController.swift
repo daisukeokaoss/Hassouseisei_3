def594a7c39b9b1411076f5e91d492d56e882620
岡　大輔（Daisuke Oka) <daisuke_oka@nanosoftware.biz>
2015-02-28 18:02:24 +0900

    Initial Commit

---
+++ Hassouseisei_ver_2_0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hassouseisei_ver_2_0
+//
+//  Created by 岡大輔 on 2015/02/28.
+//  Copyright (c) 2015年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
