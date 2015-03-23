4abaaea4ea8e434b3f69c69b9ca5332698677eeb
岡　大輔（Daisuke Oka) <daisuke_oka@nanosoftware.biz>
2015-03-23 14:19:39 +0900

    iPhoneのみに対応

--- WordPlotScrollView.swift
+++ WordPlotScrollView.swift
@@ -11,6 +11,7 @@ import UIKit
 
 
 class WordPlotScrollView: UIScrollView {
+    var generateButton:DOFlatButton!
     var Label1st:UILabel!
     var Label2nd:UILabel!
     var Label3rd:UILabel!
@@ -18,8 +19,11 @@ class WordPlotScrollView: UIScrollView {
     var Label5th:UILabel!
     var Label6th:UILabel!
     var Label7th:UILabel!
+    var Label8th:UILabel!
     
     //var generateButton:DOFlatButton
+    
+
     
 
 
@@ -30,7 +34,9 @@ class WordPlotScrollView: UIScrollView {
         // Drawing code
         //self.sizeThatFits(CGSize(width: UIScreen.mainScreen().bounds.size.width, height: UIScreen.mainScreen().bounds.size.height) )
         
-        var Row_height = UIScreen.mainScreen().bounds.size.height/10
+        print("Drowed")
+        
+        var Row_height = UIScreen.mainScreen().bounds.size.height/13
         var xCoordinateOfStartingWord = rect.width/24
         
         var LabelHeight = CGFloat(30)
@@ -49,6 +55,7 @@ class WordPlotScrollView: UIScrollView {
         self.Label5th = UILabel(frame: CGRectMake(X_Label, Row_height*5, label_width, LabelHeight))
         self.Label6th = UILabel(frame: CGRectMake(X_Label, Row_height*6, label_width, LabelHeight))
         self.Label7th = UILabel(frame: CGRectMake(X_Label, Row_height*7, label_width, LabelHeight))
+        self.Label8th = UILabel(frame: CGRectMake(X_Label, Row_height*8, label_width, LabelHeight))
         
         self.Label1st.text = "Label1st"
         self.Label2nd.text = "Label2nd"
@@ -57,6 +64,7 @@ class WordPlotScrollView: UIScrollView {
         self.Label5th.text = "Label5th"
         self.Label6th.text = "label6th"
         self.Label7th.text = "Label7th"
+        self.Label8th.text = "Label8th"
         
         self.Label1st.sizeToFit()
         
@@ -70,21 +78,28 @@ class WordPlotScrollView: UIScrollView {
         self.addSubview(self.Label5th)
         self.addSubview(self.Label6th)
         self.addSubview(self.Label7th)
+        self.addSubview(self.Label8th)
         
-      /*  self.generateButton.frame = CGRectMake(X_Label, Row_height*8, label_width,LabelHeight*2)
-        self.generateButton.faceColor = UIColor(red: 86.0/255.0, green: 161.0/255.0, blue: 217.0/255.0, alpha: 1.0)
+        self.generateButton = DOFlatButton()
+        
+        self.generateButton.frame = CGRectMake(X_Label, Row_height*9, label_width,Row_height*2.85)
+        /*self.generateButton.faceColor = UIColor(red: 86.0/255.0, green: 161.0/255.0, blue: 217.0/255.0, alpha: 1.0)
         self.generateButton.sideColor = UIColor(red: 79.0/255.0, green: 127.0/255.0, blue: 179.0/255.0, alpha: 1.0)
         self.generateButton.radius = 8.0
         self.generateButton.margin = 4.0
-        self.generateButton.depth  = 3.0
+        self.generateButton.depth  = 3.0*/
+        
+        self.generateButton.faceColor = UIColor(red: 243.0/255.0, green: 152.0/255.0, blue: 0, alpha: 1.0)
+        self.generateButton.sideColor = UIColor(red: 170.0/255.0, green: 105.0/255.0, blue: 0, alpha: 1.0)
+        self.generateButton.radius = 6.0
+        self.generateButton.margin = 7.0
+        self.generateButton.depth  = 6.0
         
         self.generateButton.titleLabel?.font = UIFont.boldSystemFontOfSize(16)
         self.generateButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
         self.generateButton.setTitle("Push", forState: UIControlState.Normal)
         
-        self.addSubview(self.generateButton)*/
-        
-        
+        self.addSubview(self.generateButton)
         
         
     }
